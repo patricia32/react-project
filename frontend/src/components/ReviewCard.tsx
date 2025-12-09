14ef879c843a4557691a80741e80291d507efe1e
Patricia Teocan <teocan.patricia03@gmail.com>
2025-12-09 15:31:34 +0200

    made review cards more responsive to long texts

--- frontend/src/components/ReviewCard.tsx
+++ frontend/src/components/ReviewCard.tsx
@@ -2,16 +2,44 @@ import type { Review } from '../types/Review';
 import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
 import dateFormat from '../utils/dateFormat';
 import Rating from '@mui/material/Rating';
-
+import { useEffect, useState } from 'react';
 interface Props {
   review: Review;
 }
-
 export default function ReviewCard({ review }: Props) {
+  const [longReview, setLongReview] = useState(true);
+  const [expanded, setExpanded] = useState(false);
+  useEffect(() => {
+    if (review.text.length < 65) setLongReview(false);
+  }, [review]);
+  function toggleSeeMore() {
+    setExpanded(!expanded);
+  }
   return (
-    <div className="reviewCard">
+    <div className={`reviewCard ${expanded ? 'expandedCard' : ''}`}>
       <FormatQuoteIcon className="reviewCard__item " fontSize="large" />
-      <div className="reviewCard__text">{review.text}</div>
+      <div className="reviewCard__wrapper">
+        <div
+          className={
+            expanded
+              ? 'reviewCard__wrapper-expandedText'
+              : 'reviewCard__wrapper-text'
+          }
+        >
+          {review.text}
+        </div>
+        <div className="reviewCard__wrapper-seeMore">
+          {longReview ? (
+            expanded ? (
+              <div onClick={toggleSeeMore}>See less</div>
+            ) : (
+              <div onClick={toggleSeeMore}>See more</div>
+            )
+          ) : (
+            ''
+          )}
+        </div>
+      </div>
       <div className="reviewCard__rating">
         <Rating
           className="reviewCard__rating-stars"
